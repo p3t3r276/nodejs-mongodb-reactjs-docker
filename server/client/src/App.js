f2b7ffbff88cef01a479519f243bd1dcff10de66
p3t3r276 <p3t3r276@gmail.com>
2021-08-24 14:43:55 +0700

    Finish setup Redux

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,18 +1,24 @@
-import AppNavBar from './components/AppNavBar';
-import ShoppingList from './components/ShoppingList';
-
 import { Provider } from 'react-redux'
+import { Container} from 'reactstrap';
 import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+
+import AppNavBar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
+
+import ItemModal from './components/ItemModal';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <AppNavBar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
---
+++ server/client/src/components/ItemModal.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, Modal, ModalBody, ModalHeader } from 'reactstrap';
+import { v4 } from 'uuid';
+import { connect } from 'react-redux';
+import { addItem } from '../actions/itemActions';
+
+class ItemModal extends Component {
+  state = {
+    moda: false,
+    name: ''
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const newItem = {
+      id: v4(),
+      name: this.state.name
+    }
+
+    this.props.addItem(newItem)
+
+    this.toggle();
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <Button
+          color="dark"
+          style={{ marginBottom: '2rem' }}
+          onClick={this.toggle}
+        >Add Item</Button>
+
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Add to shopping list</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="item">Item</Label>
+                <Input type="text"
+                  name="name"
+                  id="item"
+                  placeholder="Add shopping item"
+                  onChange={this.onChange}
+                  />
+
+              </FormGroup>
+                <Button color="dark" style={{ marginBottom: '2rem' }} block>
+                  Add Item
+                </Button>
+              </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default connect(null, { addItem })(ItemModal);
